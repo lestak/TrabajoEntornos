c6a3eddc600b15976c22aa717bb97be9aa157af7
lestak <lestatan@hotmail.com>
2017-05-16 22:08:40 +0200

    Update EntornosTrabajo.java

--- src/Entornos/EntornosTrabajo.java
+++ src/Entornos/EntornosTrabajo.java
@@ -5,7 +5,8 @@ public class EntornosTrabajo {
 	public static void main(String[] args){
 		System.out.println("AUPA AHI todo el mundo!!!!");
 
-		System.out.println("Estamos haciendo commit para subilo al repositorio!!!!");
+		System.out.println("Estamos haciendo commit para subirlo al repositorio!!!!");
+		System.out.println("he modificado el archivo en github para hacer un update");
 	}
 
 }
