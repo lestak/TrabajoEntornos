9af58ef22b1e7b00958e1f444f79e178c262c4b0
Susana <Susana@192.168.1.39>
2017-05-16 21:54:13 +0200

    ss

--- src/Entornos/EntornosTrabajo.java
+++ src/Entornos/EntornosTrabajo.java
@@ -5,7 +5,7 @@ public class EntornosTrabajo {
 	public static void main(String[] args){
 		System.out.println("AUPA AHI todo el mundo!!!!");
 
-		System.out.println("Estamos haciendo commit para subirlo al repositorio!!!!");
+		System.out.println("Estamos haciendo commit para subilo al repositorio!!!!");
 	}
 
 }
