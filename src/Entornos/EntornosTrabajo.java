289e048328104d3fc269eeab7b35983253554ae0
Susana <Susana@192.168.1.39>
2017-05-16 21:51:00 +0200

    Explicacion commit

--- src/Entornos/EntornosTrabajo.java
+++ src/Entornos/EntornosTrabajo.java
@@ -5,7 +5,7 @@ public class EntornosTrabajo {
 	public static void main(String[] args){
 		System.out.println("AUPA AHI todo el mundo!!!!");
 
-		System.out.println("Estamos haciendo commit de modo local!!!!");
+		System.out.println("Estamos haciendo commit para subirlo al repositorio!!!!");
 	}
 
 }
